d4ed52f3d5376ed1a57fadbd87bc7387ae943ebf
Karin Marjieh <karinmarjieh@hotmail.com>
2023-09-09 17:51:10 +0300

    added all files

---
+++ src/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MenuItem } from './MenuItem'
+
+export const Menu = ({items}) => {
+  return (
+    <div className='section-center'>
+    {items.map((menuItem)=>{
+        return <MenuItem key={menuItem.id}{...menuItem}/>
+    })}
+    </div>
+  )
+}
